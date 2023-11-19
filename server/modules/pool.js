8af62a54755491433d766af5ad5fe73e2b27e888
Haleigh <124629294+halziebol11@users.noreply.github.com>
2023-11-19 14:59:55 -0600

    Initial commit

---
+++ server/modules/pool.js
@@ -0,0 +1,35 @@
+/* the only line you likely need to change is
+
+ database: 'prime_app',
+
+ change `prime_app` to the name of your database, and you should be all set!
+*/
+
+const pg = require('pg');
+let pool;
+
+// When our app is deployed to the internet 
+// we'll use the DATABASE_URL environment variable
+// to set the connection info: web address, username/password, db name
+// eg: 
+//  DATABASE_URL=postgresql://jDoe354:secretPw123@some.db.com/prime_app
+if (process.env.DATABASE_URL) {
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+}
+// When we're running this app on our own computer
+// we'll connect to the postgres database that is 
+// also running on our computer (localhost)
+else {
+    pool = new pg.Pool({
+        host: 'localhost',
+        port: 5432,
+        database: 'prime_app',   // 	💥 Change this to the name of your database!
+    });
+}
+
+module.exports = pool;
