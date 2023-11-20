89f73c4eb73fa629607d6e69e8739aa499040d5c
halziebol11 <halziebol11@gmail.com>
2023-11-20 10:28:20 -0600

    updated database.sql

--- database.sql
+++ database.sql
@@ -5,6 +5,55 @@
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "email" VARCHAR (120) UNIQUE NOT NULL,
+    "admin" BOOLEAN DEFAULT 0
 );
+
+CREATE TABLE "grant_data" (
+	"id" SERIAL PRIMARY KEY,
+	"cycle_id" INT REFERENCES "grant_cycle",
+	"dept_id" VARCHAR[],
+	"reviewed" BOOLEAN DEFAULT 0,
+	"google_sheet" VARCHAR(1000)
+)
+
+CREATE TABLE "grant_cycle" (
+	"id" SERIAL PRIMARY KEY,
+	"start_date" DATE,
+	"end_date" DATE,
+	"grant_type" VARCHAR(60),
+	"complete" BOOLEAN DEFAULT 0
+)
+
+CREATE TABLE "scores" (
+    "id" SERIAL PRIMARY KEY,
+	"created_at" DATE,
+	"grant_id" INT REFERENCES "grant_data",
+	"reviewer_id" INT REFERENCES "user",
+	"assigned_by" INT REFERENCES "user",
+	new_field integer
+	score_2 integer
+	score_3 integer
+)
+
+CREATE TABLE "departments" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(200)
+)
+
+CREATE TABLE "grant_assignments" (
+    "id" SERIAL PRIMARY KEY,
+	"assigned_at" DATE,
+	"assigned_by" INT REFERENCES "user",
+	"grant_id" INT REFERENCES "grant_data",
+	"reviewer_id" INT REFERENCES "user",
+	"cycle_id" INT REFERENCES "grant_cycle",
+)
+
+CREATE TABLE "reviewers" (
+    "id" SERIAL PRIMARY KEY,
+	"reviewer_id" INT REFERENCES "user",
+	"cycle_id" INT REFERENCES "grant_cycle",
+	"available_reviews" INT,
+	"dept_id" VARCHAR[],
+)
