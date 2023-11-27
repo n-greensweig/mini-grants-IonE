f6983e1fc1466e727e93d363936c970ba20647aa
RileyAlexis <MxRileyAlexis@gmail.com>
2023-11-27 13:14:45 -0600

    Added setScores route

--- server/routes/grants.router.js
+++ server/routes/grants.router.js
@@ -88,19 +88,18 @@ router.post('/',  (req, res) => {
 });//end POST
 
 
-// PUT to complete review cycle --RILEY
-router.put('/edit/:id', (req, res) => {
-  let id = req.params.id;
-  let updatedObservation = req.body;
-  console.log('updating observation for id', id);
+// PUT to finalize review cycle --RILEY
+router.put('/finalizeCycle', (req, res) => {
+  
   if (req.isAuthenticated()) {
-    let queryText = `UPDATE "observations" 
-                  SET "species_id" = $1, "location" = $2, "photo" = $3,
-                  "notes" = $4, "date_observed" = $5, "time_stamp" = $6
-                  WHERE "id" = $7;`;
-    pool.query(queryText, [updatedObservation.species_id, updatedObservation.location, updatedObservation.photo, 
-                          updatedObservation.notes, updatedObservation.date_observed, updatedObservation.time_stamp, id])
+    console.log(req.body);
+    let cycle_name = req.body.cycle_name;
+    let queryText = `UPDATE "grant_cycle" 
+                    SET "cycle_complete" = TRUE 
+                    WHERE "cycle_name" = $1;`;
+    pool.query(queryText, [cycle_name])
     .then((result) =>{
+        console.log('Success', cycle_name);
         res.sendStatus(200);
     })
     .catch((err) => {
@@ -110,27 +109,79 @@ router.put('/edit/:id', (req, res) => {
   } else {
     res.sendStatus(401);
   }
-})// end PUT
+}
+)// end PUT
 
 // PUT to set scores as reviewer --RILEY
-router.put('/edit/:id', (req, res) => {
-    let id = req.params.id;
-    let updatedObservation = req.body;
-    console.log('updating observation for id', id);
+router.put('/setScores', (req, res) => {
+
     if (req.isAuthenticated()) {
-      let queryText = `UPDATE "observations" 
-                    SET "species_id" = $1, "location" = $2, "photo" = $3,
-                    "notes" = $4, "date_observed" = $5, "time_stamp" = $6
-                    WHERE "id" = $7;`;
-      pool.query(queryText, [updatedObservation.species_id, updatedObservation.location, updatedObservation.photo, 
-                            updatedObservation.notes, updatedObservation.date_observed, updatedObservation.time_stamp, id])
-      .then((result) =>{
-          res.sendStatus(200);
+      let queryText = `SELECT * FROM "scores"
+                   WHERE "grant_id" = $1 AND "reviewer_id" = $2
+                   RETURNING id;`;
+
+      let grant_id = req.body.grant_id;
+      let reviewer_id = req.body.reviewer_id;
+      let score_id = null;
+      const created_at = req.body.created_at;
+      const assigned_by = req.body.assigned_by;
+      const interdisciplinary = req.body.interdisciplinary;
+      const goals = req.body.goals;
+      const method_and_design = req.body.method_and_design;
+      const budget = req.body.budget;
+      const impact = req.body.impact;
+      const review_complete = req.body.review_complete;
+
+      pool.query(queryText, [grant_id, reviewer_id])
+      .then((result) => {
+            if (result.rows[0]) { score_id = result.rows[0].id; }
       })
       .catch((err) => {
           console.log(`Error making query ${queryText}`, err);
-          res.sendStatus(500)
-      })
+          res.sendStatus(500)   
+      });
+
+      if (score_id !== null || score_id === undefined) {
+        queryText = `UPDATE "scores"
+                    SET "created_at" = $1,
+                        "interdisciplinary" = $2,
+                        "goals" = $3,
+                        "method_and_design = $4,
+                        "budget" = $5,
+                        "impact" = $6,
+                        "review_complete = $7,
+                    WHERE "id" = $8;`;
+        pool.query(queryText, [created_at, interdisciplinary, goals, method_and_design, budget, impact, review_complete])
+        .then((response) => {
+            res.send(201);
+        }).catch((error) => {
+            console.log(`error making query ${queryText}`);
+            res.send(500);
+        })
+      } else { 
+        queryText = `INSERT INTO "scores" (
+                        "created_at", 
+                        "grant_id", 
+                        "reviewer_id", 
+                        "assigned_by", 
+                        "interdisciplinary", 
+                        "goals",
+                        "method_and_design",
+                        "budget",
+                        "impact",
+                        "review_complete")
+
+                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);`;
+
+        pool.query(queryText, [created_at, grant_id, reviewer_id, assigned_by, interdisciplinary, goals, method_and_design, budget, impact, review_complete])
+        .then((response) => {
+            res.send(201);
+        }).catch((error) => {
+            console.log(`error making query ${queryText}`);
+            res.send(500);
+        })
+      }
+
     } else {
       res.sendStatus(401);
     }
