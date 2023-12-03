1bdbee222a0e1922bb1b15bcb0036d250911cbe2
Jeffrey Varughese <jeffvarughese@gmail.com>
2023-12-03 14:22:34 -0600

    changed styling, fixed typos

--- src/components/GrantReviewForm/GrantReviewForm.jsx
+++ src/components/GrantReviewForm/GrantReviewForm.jsx
@@ -107,10 +107,18 @@ function GrantReviewForm() {
                 <h3><span>Welcome </span><span><i>Reviewer</i></span></h3>
                 <p>This form is unique to each reviewer. Please use the review guidance criteria for each category below to review the proposal.</p>
                 
-                <h4><u>Project PI</u></h4>
-                    <p>PI Name</p>
-                <h4><u>Project Title</u></h4>
-                    <p>Project Title</p>
+                <div id="title">
+
+                    <div>
+                        <h4><u>Project PI</u></h4>
+                        <p>PI Name</p>
+                    </div>
+                    <div>
+                        <h4><u>Project Title</u></h4>
+                        <p>Project Title</p>
+                    </div>
+                </div>
+
             </div>
 
             <br/>
@@ -121,7 +129,7 @@ function GrantReviewForm() {
                         <th colspan="2">Interdisciplinary Collaboration</th>
                     </tr>
                     <tr id="first">
-                        <td><input type="radio" id="5" name="interdisciplinary" value={interdisciplinary} onChange={interdisciplinaryRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="5" name="interdisciplinary" value={interdisciplinary} onChange={interdisciplinaryRadioChange}/></td>
                         <td>
                             <span id="points">5 pts</span> - This proposal includes both individuals from the university and external 
                             (non-university) collaborators that represent an exemplary variety of disciplines, expertise, and ways of 
@@ -130,7 +138,7 @@ function GrantReviewForm() {
                         </td>
                     </tr>
                     <tr id="second">
-                        <td><input type="radio" id="3" name="interdisciplinary" value={interdisciplinary} onChange={interdisciplinaryRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="3" name="interdisciplinary" value={interdisciplinary} onChange={interdisciplinaryRadioChange}/></td>
                         <td>
                             <span id="points">3 pts</span> - This proposal includes individuals from within the university as well as 
                             external (non-university) stakeholders or experts, each of whom add well-articulated value to the project. 
@@ -139,7 +147,7 @@ function GrantReviewForm() {
                         </td>
                     </tr>
                     <tr id="third">
-                        <td><input type="radio" id="2" name="interdisciplinary" value={interdisciplinary} onChange={interdisciplinaryRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="2" name="interdisciplinary" value={interdisciplinary} onChange={interdisciplinaryRadioChange}/></td>
                         <td>
                             <span id="points">2 pts</span> - This proposal includes individuals from the university from different 
                             departments, units, organizations, and/or affiliations (faculty,staff, students), each of whom add value to 
@@ -147,14 +155,14 @@ function GrantReviewForm() {
                         </td>
                     </tr>
                     <tr id="second-to-last">
-                        <td><input type="radio" id="1" name="interdisciplinary" value={interdisciplinary} onChange={interdisciplinaryRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="1" name="interdisciplinary" value={interdisciplinary} onChange={interdisciplinaryRadioChange}/></td>
                         <td>
                             <span id="points">1 pts</span> - This proposal appears to be interdisciplinary based on team members' 
                             affiliation, but one or more of the partners' roles is poorly defined or does not appear to add value.
                         </td>
                     </tr>
                     <tr id="last">
-                        <td><input type="radio" id="0" name="interdisciplinary" value={interdisciplinary} onChange={interdisciplinaryRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="0" name="interdisciplinary" value={interdisciplinary} onChange={interdisciplinaryRadioChange}/></td>
                         <td>
                             <span id="points">0 pts</span> - This proposal does not appear to be interdisciplinary; only one unit
                             or discipline is represented, and no external partners are present.
@@ -169,7 +177,7 @@ function GrantReviewForm() {
                         <th colspan="2">Project Goals</th>
                     </tr>
                     <tr id="first">
-                        <td><input type="radio" id="5" name="goals" value={goals} onChange={goalsRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="5" name="goals" value={goals} onChange={goalsRadioChange}/></td>
                         <td>
                             <span id="points">5 pts</span> - Illustrates a powerful approach or solution to a compelling problem or 
                             opportunity; goals are clear and attainable. Connection to the environment or sustainability is explicit 
@@ -177,28 +185,28 @@ function GrantReviewForm() {
                         </td>
                     </tr>
                     <tr id="second">
-                        <td><input type="radio" id="3" name="goals" value={goals} onChange={goalsRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="3" name="goals" value={goals} onChange={goalsRadioChange}/></td>
                         <td>
                             <span id="points">3 pts</span> - Provides an interesting or novel approach to a compelling problem or 
                             opportunity; goals are clear and attainable. Connection to the environment or sustainability is clear.
                         </td>
                     </tr>
                     <tr id="third">
-                        <td><input type="radio" id="2" name="goals" value={goals} onChange={goalsRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="2" name="goals" value={goals} onChange={goalsRadioChange}/></td>
                         <td>
                             <span id="points">2 pts</span> - Goals and/or outcomes are clear, but their relationship to the 
                             problem is vague. Proposal appears to have a relationship to environment or sustainability issues.
                         </td>
                     </tr>
                     <tr id="second-to-last">
-                        <td><input type="radio" id="1" name="goals" value={goals} onChange={goalsRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="1" name="goals" value={goals} onChange={goalsRadioChange}/></td>
                         <td>
                             <span id="points">1 pts</span> - Proposal appears to have a relationship to environment or sustainability 
                             issues, but the goals and/or outcomes are not clearly articulated.
                         </td>
                     </tr>
                     <tr id="last">
-                        <td><input type="radio" id="0" name="goals" value={goals} onChange={goalsRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="0" name="goals" value={goals} onChange={goalsRadioChange}/></td>
                         <td>
                             <span id="points">0 pts</span> - Communicated goals and/or outcomes do
                             not relate to environmental or sustainability
@@ -214,21 +222,21 @@ function GrantReviewForm() {
                         <th colspan="2">Method/Design</th>
                     </tr>
                     <tr id="first">
-                        <td><input type="radio" id="5" name="method_and_design" value="method_and_design" onChange={method_and_designRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="5" name="method_and_design" value="method_and_design" onChange={method_and_designRadioChange}/></td>
                         <td>
                             <span id="points">5 pts</span> - All aspects of this cohesive project are thoroughly articulated and 
                             logically connected. Purpose and impact are clear.
                         </td>
                     </tr>
                     <tr id="second">
-                        <td><input type="radio" id="3" name="method_and_design" value="method_and_design" onChange={method_and_designRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="3" name="method_and_design" value="method_and_design" onChange={method_and_designRadioChange}/></td>
                         <td>
                             <span id="points">3 pts</span> - Most aspects of the project or proposal are thoroughly articulated and 
                             logically connected; purpose and/or impact is mostly clear.
                         </td>
                     </tr>
                     <tr id="second-to-last">
-                        <td><input type="radio" id="2" name="method_and_design" value="method_and_design" onChange={method_and_designRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="2" name="method_and_design" value="method_and_design" onChange={method_and_designRadioChange}/></td>
                         <td>
                             <span id="points">2 pts</span> - Some aspects of this project or proposal are well articulated
                             and connected, but the proposal struggles to connect all the dots. Purpose and/or impact is not
@@ -236,7 +244,7 @@ function GrantReviewForm() {
                         </td>
                     </tr>
                     <tr id="last">
-                        <td><input type="radio" id="0" name="method_and_design" value="method_and_design" onChange={method_and_designRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="0" name="method_and_design" value="method_and_design" onChange={method_and_designRadioChange}/></td>
                         <td>
                             <span id="points">0 pts</span>- Almost all aspects of the project design are incoherent; purpose and/or 
                             impact is unclear.
@@ -251,27 +259,27 @@ function GrantReviewForm() {
                         <th colspan="2">Budget</th>
                     </tr>
                     <tr id="first">
-                        <td><input type="radio" id="2" name="budget" value="budget" onChange={budgetRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="2" name="budget" value="budget" onChange={budgetRadioChange}/></td>
                         <td>
                             <span id="points">2 pts</span> - Budget is accurate, detailed, cost-effective, and crafted to fully 
                             support the scope of work.
                         </td>
                     </tr>
                     <tr id="second">
-                        <td><input type="radio" id="1" name="budget" value="budget" onChange={budgetRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="1" name="budget" value="budget" onChange={budgetRadioChange}/></td>
                         <td>
                             <span id="points">1 pts</span> - Budget is accurate and detailed, but it may inadequately support the scope
                             of work. The project may need to scale up or down.
                         </td>
                     </tr>
                     <tr id="second-to-last">
-                        <td><input type="radio" id="0.5" name="budget" value="budget" onChange={budgetRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="0.5" name="budget" value="budget" onChange={budgetRadioChange}/></td>
                         <td>
                             <span id="points">0.5 pts</span>- Budget lacks sufficient detail.
                         </td>
                     </tr>
                     <tr id="last">
-                        <td><input type="radio" id="0" name="budget" value="budget" onChange={budgetRadioChange}/></td>
+                        <td className="radio-td"><input type="radio" id="0" name="budget" value="budget" onChange={budgetRadioChange}/></td>
                         <td>
                             <span id="points">0 pts</span> - Budget is incomplete, over $3,000, or contains unallowable expenses 
                             includingfaculty / staff salary, alcohol, or other banned expenses per UMN policy.
@@ -286,13 +294,13 @@ function GrantReviewForm() {
                         <th colspan="2">Impact <i>(optional)</i></th>
                     </tr>
                     <tr id="white">
-                        <td><input type="checkbox" id="2" name="impact1" value="impact1" onChange={(e) => setImpact1(e.target.checked ? e.target.id : 0)}/></td>
+                        <td className="radio-td"><input type="checkbox" id="2" name="impact1" value="impact1" onChange={(e) => setImpact1(e.target.checked ? e.target.id : 0)}/></td>
                         <td>
                             <span id="points">2 pts</span> - Meaningfully addresses diversity, equity, inclusion and/or justice.
                         </td>
                     </tr>
                     <tr id="white">
-                        <td><input type="checkbox" id="2" name="impact2" value="impact2" onChange={(e) => setImpact2(e.target.checked ? e.target.id : 0)}/></td>
+                        <td className="radio-td"><input type="checkbox" id="2" name="impact2" value="impact2" onChange={(e) => setImpact2(e.target.checked ? e.target.id : 0)}/></td>
                         <td>
                             <span id="points">2 pts</span> - Clearly aligns with one or more IonE Impact Goals: 
                                 <ul>
@@ -303,7 +311,7 @@ function GrantReviewForm() {
                         </td>
                     </tr>
                     <tr id="white">
-                        <td><input type="checkbox" id="2" name="impact3" value="impact3" onChange={(e) => setImpact3(e.target.checked ? e.target.id : 0)}/></td>
+                        <td className="radio-td"><input type="checkbox" id="2" name="impact3" value="impact3" onChange={(e) => setImpact3(e.target.checked ? e.target.id : 0)}/></td>
                         <td>
                             <span id="points">2 pts</span> - Clearly aligns with UMN Systemwide Strategic Plan, specifically the 
                             MNtersections initiative: 
@@ -323,16 +331,18 @@ function GrantReviewForm() {
                         <th colspan="2">Final Recommendation</th>
                     </tr>
                     <tr id="first">
-                        <td><input type="radio" id="3" name="recommendation" value={recommendation} onChange={recommendationRadioChange}/></td>
-                        <td><span id="points">3 pts</span><span id="points"></span>- Proposal recommended for full funding.</td>
+                        <td className="radio-td"><input type="radio" id="3" name="recommendation" value={recommendation} onChange={recommendationRadioChange}/></td>
+                        <td><span id="points">3 pts</span><span id="points"></span> - Proposal recommended for full funding.</td>
                     </tr>
                     <tr id="second">
-                        <td><input type="radio" id="2" name="recommendation" value={recommendation} onChange={recommendationRadioChange}/></td>
-                        <td><span id="points">2 pts</span> - Proposal should be funded in accordance withavailable resources.</td>
+                        <td className="radio-td"><input type="radio" id="2" name="recommendation" value={recommendation} onChange={recommendationRadioChange}/></td>
+                        <td><span id="points">2 pts</span> - Proposal should be funded in accordance with available resources.</td>
                     </tr>
                     <tr id="last">
-                        <td><input type="radio" id="0" name="recommendation" value={recommendation} onChange={recommendationRadioChange}/></td>
-                        <td><span id="points">0 pts</span> - Proposal not recommended for funding.</td>
+                        <td className="radio-td"><input type="radio" id="0" name="recommendation" value={recommendation} onChange={recommendationRadioChange}/></td>
+                        <td> 
+                            <span id="points">0 pts</span> - Proposal not recommended for funding.
+                        </td>
                     </tr>
                 </table>
             </form>
