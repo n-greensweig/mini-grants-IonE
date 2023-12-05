581751a1c0b8b05b3f7f1838685927f0554b9d55
Jeffrey Varughese <jeffvarughese@gmail.com>
2023-12-05 15:55:49 -0600

    added component for Scored Review Details

---
+++ src/components/ScoredReviewDetails/ScoredReviewDetails.jsx
@@ -0,0 +1,17 @@
+// React
+import React, { useState} from 'react';
+import { useHistory } from 'react-router-dom';
+
+// Axios
+import axios from 'axios';
+
+// Styles
+import './ScoredReviewDetails.css'
+
+
+function ScoredReviewDetails() {
+
+}
+
+
+export default ScoredReviewDetails;
