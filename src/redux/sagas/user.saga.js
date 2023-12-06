3e86f689f795f21895f255ba36c425723a58a329
Haleigh <halziebol11@gmail.com>
2023-12-06 14:53:19 -0600

    set up cycleID call to set reducer

--- src/redux/sagas/user.saga.js
+++ src/redux/sagas/user.saga.js
@@ -20,7 +20,6 @@ function* fetchUser() {
     // with an id and username set the client-side user object to let
     // the client-side code know the user is logged in
     yield put({ type: 'SET_USER', payload: response.data });
-    yield put({type:'FETCH_CURRENT_CYCLE'})
   } catch (error) {
     console.log('User get request failed', error);
   }
